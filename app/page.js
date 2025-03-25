6cf2287f074fb41ff5389375c04657c15a405a36
Shaharia hasan <hshaharia064@gmail.com>
2025-03-26 00:58:11 +0530

    learned aboout SSR,SSG,ISR in next js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
-import Image from "next/image";
 
-export default function Home() {
-  return (
-   <div>
-    Hello
-   </div>
-  );
+
+export default async function Page(){
+const data = await fetch('https://api.vercel.app/blog' , {next : {revalidate: 3600}})
+const posts = await data.json()
+  return(
+    <div>
+      {posts.map((post)=>(
+        <li key={post.id}> {post.title}</li>
+
+      ))}
+    </div>
+
+  )
 }
